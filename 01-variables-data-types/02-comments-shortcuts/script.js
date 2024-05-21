ee6df61ba51bcf52dd7b02988437ed31fbc11a3e
Toni <antostr.dev@gmail.com>
2024-05-21 17:37:18 +0200

    aggiunta shortcut

---
+++ 01-variables-data-types/02-comments-shortcuts/script.js
@@ -0,0 +1,53 @@
+// Questo e' una linea di commento e non ha bisogno di chiusura poiche' dura solo una riga.
+
+/* Questo e' un blocco commento 
+
+Come vedi e' ancora grigio 
+
+E cosi' si chiude.
+*/
+
+// Oppure premendo "Ctrl + /" e ti seleziona in automatico la riga commentandola oppure decommentandola.
+
+
+console.log('test');
+console.log('test');
+
+console.log('test');
+console.log('test');
+
+console.log('test');
+console.log('test');
+
+console.log('test');
+
+console.log('test');
+
+// Per selezionare senza usare il mouse puoi portarti alla fine di una riga ed andando su o giu' con "Shift + freccia su, giu, destra o sinistra" selezionando e commentando con "Ctrl + /".
+
+// Puoi usare "Ctrl + destra o sinistra per muoverti nel codice".
+
+// Puoi selezionare il testo con "Shift + Ctrl + freccette".
+
+// Puoi muovere una linea di codice mettiti dentro alla linea e premi "Alt + su o giu'"
+
+// Puoi duplicare una linea di codice mettendoti dentro e premendo "Shift + Alt + su o giu' ".
+
+console.log('test');
+
+/* Posso selezionare tutto cio' che contiene la mia selezione premo "Ctrl + D" per esempio mi metto su (log) e continuando a premere lui mi selezionera' piano piano dipende da quanti ce ne sono e come premo i vari (log).
+
+Per selezionare tutti i log (in questo caso ma con qualsiasi cosa) faccio "Ctrl + Shift + L".
+
+Posso impostare un cursore con "Alt + Click Destro" per creare un inizio di una linea di codice in una linea esistente.
+*/
+
+// Posso cercare un file premendo "Ctrl + Shift + O" ed il file che sto cercando per aprirlo in automatico senza cercarlo manualmente.
+
+// Per cercare invece parole o frasi in un documento faccio "Ctrl + F" ed immetto la cosa che voglio cercare.
+
+// QUESTA UTILE
+// "Ctrl + B" per chiudere ed aprire la barra si sinistra.
+
+// Per aprire il terminale fai "Ctrl + ~" 
+// Non penso ci sia un modo per farla senza tastierino numerico e meno che non setti i tasti diversamente etc.
