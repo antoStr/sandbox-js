df9d1f767b9241196fd6e49eb4f657412664683d
antoStr <antostr.dev@gmail.com>
2024-05-22 16:37:50 +0200

    data add c

---
+++ 01-variables-data-types/04-data-types/script.js
@@ -0,0 +1,28 @@
+/* TIPI DI DATI PRIMITIVI
+
+
+String = Sequenza di caratteri
+
+Number = Numeri interi o con punto
+
+Boolean = si o no
+
+Null = significa che è nullo (Valore apposta fatto nullo)
+
+Undefined = significa che non è definito (Valore non fatto apposta)
+
+Symbol = costrutti che ritornano ad un simbolo (advanced)
+
+BigInt = numeri grandi che il valore number non può controllare.
+
+Reference Types = Objects
+Sono valori che assegnamo ad un valore non primitivo come una variabile oppure un array, funzioni etc.
+*/
+
+const nome = 'Ciccio';
+
+const output = nome
+
+// aggiungo type of per verificare che tipo di cacata è
+
+console.log(output, typeof output);
