78ef7de5b52e1a784aea06fbc77d80316c5bfa87
antoStr <antostr.dev@gmail.com>
2024-05-23 15:13:36 +0200

    stack / heap

---
+++ 01-variables-data-types/05-stack-heap/script.js
@@ -0,0 +1,4 @@
+// I Primitive Types sono messi nello 'stack' della memoria (string, boolean, Number etc.)
+// I Reference Types sono messi nello 'heap' della memoria (array, funzioni ed oggetti)
+
+// Lo stack è la memoria generale e per prendere dettagli deve andarsi a pescare i dettagli dall'heap dove c'è la funzione. Quindi in un certo senso stack ed heap sono collegati ed interagiscono fra loro.
