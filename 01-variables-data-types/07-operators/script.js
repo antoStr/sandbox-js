128fffb94e0b5c006946c783afe73fdff3470e0c
Toni <antostr.dev@gmail.com>
2024-05-24 00:27:13 +0200

    operatori

--- 01-variables-data-types/07-operators/script.js
+++ 01-variables-data-types/07-operators/script.js
@@ -1,7 +1,59 @@
 // 1. Arithmetic Operators
 
+let x;
+
+x = 5 + 5
+x = 5 - 5
+x = 5 * 5
+x = 5 / 5
+x = 7 % 5
+
+// Concatenazione
+x = 'Hello' + ' ' + 'Mondo';
+
+// Elevamento a potenza
+x = 2**3;
+
+// Incremento
+x = 1
+// x = x + 1
+x++;
+
+// Decremento
+// x = x + 1
+x--;
 
 // 2. Assignment Operators
 
+x = 10;
+
+x += 5;
+x -= 5;
+x *= 5;
+x %= 5;
+x **= 5;
 
 // 3. Comparison Operators
+
+// =      =>      assegno un valore
+// ==     =>      sto paragonando/valutando due cose
+
+
+x = 2 == 2;  // == controlla solo il valore e non il tipo di dato ex.    x = 2 == '2'   => true  
+
+x = 2 === 2; // == controlla valore ed anche tipo di dato         ex.    x = 2 === '2'  => false
+
+// ! = non e' uguale
+
+x = 2 != 2;
+x = 2 !== '2';
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
+
+
+
+
+console.log(x);
