6a45920d42ac65472237dc31c7094b497a2f0520
Toni <antostr.dev@gmail.com>
2024-05-24 23:30:18 +0200

    init ex

---
+++ 01-variables-data-types/13-number-challenge/script.js
@@ -0,0 +1,17 @@
+let x, y, sum, dif, prod, quot, rm;
+
+x = Math.floor(Math.random() * 100 + 1);
+y = Math.floor(Math.random() * 50 + 1);
+
+sum = x + y;
+dif = x - y;
+prod = x * y;
+quot = x / y;
+// rm = x % y;
+
+console.log('Sum is equal to:', sum,' - ', x, y);
+console.log('Difference is equal to:', dif,' - ', x, y);
+console.log('Product is equal to:', prod,' - ', x, y);
+console.log('Quotient is equal to:',' - ', quot, x, y);
+// console.log('Remainder is equal to:',' - ', rm, x, y);
+
