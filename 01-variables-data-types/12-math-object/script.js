1a51656a8d77cfc75786123aaaf10d49cabe90a2
Toni <antostr.dev@gmail.com>
2024-05-24 23:07:14 +0200

    math operators

---
+++ 01-variables-data-types/12-math-object/script.js
@@ -0,0 +1,30 @@
+let x;
+
+x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x = Math.round(4.2);   // arrotondamento
+
+x = Math.ceil(4.2);    // arrotonda sempre per eccesso
+x = Math.floor(4.9);   // arrotonda sempre per difetto
+
+x = Math.pow(2, 3);    // 2 elevato a 3  -   x a y
+
+x = Math.min(4, 5, 3); // output del piu' piccolo nel gruppo
+x = Math.max(4, 5, 3); // output del piu' grande nel gruppo
+
+x = Math.random();                         // numero random fra 0 ed 1 (default)
+x = Math.floor(Math.random() * 10 + 1);    // numero random fra 1 e 10
+
+
+
+
+
+
+
+
+
+
+
+console.log(x);
