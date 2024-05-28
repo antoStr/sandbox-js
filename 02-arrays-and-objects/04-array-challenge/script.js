0e80cd1645593455753eec1ae6b68be77d946578
Toni <antostr.dev@gmail.com>
2024-05-29 01:58:00 +0200

    challenge 1-2 arr

---
+++ 02-arrays-and-objects/04-array-challenge/script.js
@@ -0,0 +1,19 @@
+// Challenge 1
+const arr = [1, 2, 3, 4, 5];
+// [6, 5, 4, 3, 2, 1, 0];
+
+arr.push(6);
+arr.unshift(0);
+arr.reverse();
+// console.log(arr);
+
+//Challenge 2
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9, 10];
+// [1,2,3,4,5,6,7,8,9,10]
+
+arr1.pop(4);
+const x = arr1.concat(arr2);
+
+console.log(x);
+
