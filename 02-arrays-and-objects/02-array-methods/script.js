29cce9fd201b42a49875986a71f33f9a4b4f4e7b
Toni <140510760+antoStr@users.noreply.github.com>
2024-05-28 17:53:45 +0200

    array e metodi

--- 02-arrays-and-objects/02-array-methods/script.js
+++ 02-arrays-and-objects/02-array-methods/script.js
@@ -15,9 +15,26 @@ arr.shift();
 
 // Capovolge o reversa l'array
 arr.reverse();
+arr.reverse();
 
 // Controlla se c'è o non c'è un valore
 x = arr.includes(7);
 
-x = arr.indexOf(8);
-console.log(arr, x);
+// Restituisce il valore della posizione es. n^5 = 4^a posizione
+// -1 significa che non esiste
+x = arr.indexOf(5);
+
+// taglia una porzione dell'array
+// solo un valore prende i valori seguenti al valore della posizione che gli diamo
+x = arr.slice(1,4);
+
+// taglia una porzione di array cambiando anche il vecchio
+// x = arr.splice(2,4);
+
+// taglia un elemento (pos. 2 e ne taglio 1 solo) anche dall'array iniziale
+// x = arr.splice(2, 1);
+
+// taglia una porzione, reversa, converte in stringa e restituisce un valore con charAt che è una funzione per le stringhe
+x = arr.splice(1,4).reverse().toString().charAt();
+
+console.log(x);
